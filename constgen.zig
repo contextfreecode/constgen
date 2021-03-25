dbac3606d84c06d750c142caf123f55a40392348
Tom <tom@contextfree.info>
2021-03-25 08:03:19 -0700

    Implement dynamic length dot

--- constgen.zig
+++ constgen.zig
@@ -1,7 +1,15 @@
 const std = @import("std");
 
+fn dot(a: []const f64, b: []const f64) f64 {
+    var result: f64 = 0;
+    for (a) |val_a, index| {
+        result += val_a * b[index];
+    }
+    return result;
+}
+
 pub fn main() !void {
     const a = [_]f64{ 1.5, 2 };
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello, {}!\n", .{"world"});
+    try stdout.print("Hello, {}!\n", .{dot(&a, &a)});
 }
