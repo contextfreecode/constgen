938b6461aa5b9a11510241e03c2fb847190beb2f
Tom <tom@contextfree.info>
2021-03-27 06:01:59 -0700

    Extract norm2f64

--- constgen.nim
+++ constgen.nim
@@ -10,6 +10,9 @@ func dot[Size: static int, Val](a, b: array[Size, Val]): Val =
 func norm[Size: static int, Val](a: array[Size, Val]): Val =
   dot(a, a).sqrt()
 
+# func norm2f64(x, y: float64): float64 {.exportc.} =
+#   norm([x, y])
+
 proc main() =
   var a = [1.5, 2]
   echo fmt"norm: {norm(a)}"
