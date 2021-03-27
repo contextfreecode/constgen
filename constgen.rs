8c21641f567bcc4a65e094c0f42e57b46390b6cb
Tom <tom@contextfree.info>
2021-03-26 21:52:55 -0700

    Implement rust

--- constgen.rs
+++ constgen.rs
@@ -1,3 +1,20 @@
+use num_traits::Float;
+
+fn dot<Val, const SIZE: usize>(a: &[Val; SIZE], b: &[Val; SIZE]) -> Val
+where
+    Val: Float,
+{
+    a.iter()
+        .zip(b)
+        .map(|(x, y)| *x * *y)
+        .fold(Val::zero(), |sum, xy| sum + xy)
+}
+
+fn norm<Val: Float, const SIZE: usize>(a: &[Val; SIZE]) -> Val {
+    dot(a, a).sqrt()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = [1.5, 2.0];
+    println!("norm: {}", norm(&a));
 }
