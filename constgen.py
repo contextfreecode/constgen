379749b89ab6cfd03797aac21929b71bf07b0dc9
Tom <tom@contextfree.info>
2021-03-27 13:14:19 -0700

    Maybe close in on python

--- constgen.py
+++ constgen.py
@@ -1,17 +1,36 @@
+from math import sqrt
 from typing import Tuple, TypeVar, cast
 
 
 Val = TypeVar("Val")
 
 
-def norm_gen(size: int):
+def vec_ops(size: int):
     # Vec = Tuple.__getitem__((Val,) * size)
     Vec = Tuple[Val, Val]
 
-    def norm(vec: Vec) -> Val:
-        val = cast[Val](0)
-        for i in range(size):
-            val += vec[i] * vec[i]
-        return val / size
+    class VecOps:
+        @staticmethod
+        def dot(a: Vec, b: Vec) -> Val:
+            val = cast(Val, 0)
+            for i in range(size):
+                val += a[i] * b[i]
+            return val
 
-    return norm
+        @staticmethod
+        def norm(a: Vec) -> Val:
+            return cast(Val, sqrt(VecOps.dot(a, a)))
+
+    return VecOps
+
+
+vec_ops_2 = vec_ops(2)
+
+
+def main():
+    a = (1.5, 2)
+    print(f"norm: {vec_ops_2.norm(a)}")
+
+
+if __name__ == "__main__":
+    main()
