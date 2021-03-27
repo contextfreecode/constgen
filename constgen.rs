d4fb4c1beb80bbd3061463176c2b6c899159bdad
Tom <tom@contextfree.info>
2021-03-26 22:10:46 -0700

    And more

--- constgen.rs
+++ constgen.rs
@@ -2,7 +2,7 @@ use num_traits::Float;
 use std::array::IntoIter;
 // use std::ops::*;
 
-fn dot<Val, const SIZE: usize>(a: &[Val; SIZE], b: &[Val; SIZE]) -> Val
+fn dot<Val, const SIZE: usize>(a: [Val; SIZE], b: [Val; SIZE]) -> Val
 where
     Val: Float,
 {
@@ -10,23 +10,24 @@ where
     //     .zip(b)
     //     .map(|(x, y)| *x * *y)
     //     .fold(Val::zero(), |sum, xy| sum + xy)
-    IntoIter::new(*a)
-        .zip(IntoIter::new(*b))
+    IntoIter::new(a)
+        .zip(IntoIter::new(b))
         .map(|(x, y)| x * y)
         .fold(Val::zero(), |sum, xy| sum + xy)
 }
 
-fn norm<Val: Float, const SIZE: usize>(a: &[Val; SIZE]) -> Val {
+fn norm<Val: Float, const SIZE: usize>(a: [Val; SIZE]) -> Val {
     dot(a, a).sqrt()
 }
 
-pub fn norm2(x: f64, y: f64) -> f64 {
-    return norm(&[x, y]);
-}
+// pub fn norm2(x: f64, y: f64) -> f64 {
+//     return norm([x, y]);
+// }
 
 fn main() {
     let a = [1.5, 2.0];
-    println!("norm: {}", norm(&a));
+    println!("norm: {}", norm(a));
+    println!("norm: {}", norm(a));
 }
 
 // trait Float: Copy + Sized + Add<Self, Output = Self> + Mul<Self, Output = Self> {
@@ -44,3 +45,5 @@ fn main() {
 //         0f64
 //     }
 // }
+
+// -C opt-level=3
