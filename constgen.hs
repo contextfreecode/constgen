97f3e552652af1561f0d04de40027ad8a3c94966
Tom <tom@contextfree.info>
2021-03-29 05:28:53 -0700

    Explore python and hs

--- constgen.hs
+++ constgen.hs
@@ -1,1 +1,18 @@
-main = putStrLn "Hi!"
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+-- This is unfinished exploration. I'm not even sure I'm on the right path.
+
+import Data.Array.Unboxed
+import GHC.TypeLits
+
+-- data Vec2 (size::Nat)
+newtype Vec (size :: Nat) val = Vec (UArray Int val)
+
+hi :: Vec size Double -> Double
+hi (Vec a) = a ! 1
+
+main =
+  let a = Vec (array (0, 1) [(0, 1.5), (1, 2.0)] :: UArray Int Double)
+  in putStrLn (show (hi a))
--- constgen.py
+++ constgen.py
@@ -4,14 +4,17 @@ from typing import Any, Callable, Tuple, TypeVar, cast
 
 
 Val = TypeVar("Val")
-# Vec = Tuple.__getitem__((Val,) * size)
-# print(Vec)
 # Vec = Tuple[Val, Val]
 # print(Vec)
 Vec = Sequence[Val]
 
 
 def vec_ops(*, size: int, val_type: Callable[[Any], Val]):
+
+    # Vec = Tuple.__getitem__((Val,) * size)
+    # print(Vec)
+    # Vec = Tuple.__getitem__((val_type,) * size)
+    # print(Vec)
 
     class VecOps:
         @staticmethod
@@ -23,6 +26,7 @@ def vec_ops(*, size: int, val_type: Callable[[Any], Val]):
 
         @staticmethod
         def norm(a: Vec) -> Val:
+            # del VecOps.dot
             return sqrt(cast(Any, VecOps.dot(a, a)))
 
     return VecOps
