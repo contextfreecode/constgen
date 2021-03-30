25155ee61eb931db75eac8b005e5780a9e068f2d
Tom <tom@contextfree.info>
2021-03-29 19:43:41 -0700

    Get crazier

--- simple.ml
+++ simple.ml
@@ -1,18 +1,22 @@
 module type Val_type = sig
-  type t
+  module X : sig
+    type t
+  end
 end
 
 module Hi (Val : Val_type) = struct
-  type t = Val.t
+  (* type t = Val.t *)
+  module X = Val.X
 end
 
 module FloatHi = Hi (struct
-  include Float
+  module X = Float
+  (* include Float *)
   (* include Int *)
 end)
 
 let main () =
-  let a : FloatHi.t = 1.0 in
+  let a : FloatHi.X.t = 1.0 in
   Printf.printf "hi: %f\n" a
 
 let () = main ()
