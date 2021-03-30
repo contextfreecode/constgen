09b2b2718b43948d9acfe35e0caaad903c4e312d
Tom <tom@contextfree.info>
2021-03-29 20:05:38 -0700

    Get float working

--- constgen.ml
+++ constgen.ml
@@ -12,25 +12,17 @@ module type Vec_type = sig
   module Val : Val_type
 end
 
-module type VecOps_type = sig
-  module Vec : Vec_type
-  val dot : Vec.Val.t array -> Vec.Val.t array -> Vec.Val.t
-  val norm : Vec.Val.t array -> Vec.Val.t
-end
-
-module VecOps (Vec : Vec_type) : VecOps_type = struct
-  open Vec.Val
-  module Vec = Vec
+module VecOps (Vec : Vec_type) = struct
+  include Vec
   let dot a b =
     (* Loop on int range just to show we can use the size. *)
-    let range = Array.init Vec.size succ in
-    let prods = Array.map (fun i -> mul a.(i - 1) b.(i - 1)) range in
-    Array.fold_left (fun x sum -> add x sum) zero prods
-  let norm a = sqrt (dot a a)
+    let range = Array.init size succ in
+    let prods = Array.map (fun i -> Val.mul a.(i - 1) b.(i - 1)) range in
+    Array.fold_left (fun x sum -> Val.add x sum) Val.zero prods
+  let norm a = Val.sqrt (dot a a)
 end
 
 module VecOps2c = VecOps (struct
-  open Complex
   module Val : Val_type = struct
     include Complex
     let to_string c = Printf.sprintf "%f + %f im" c.re c.im
@@ -50,6 +42,6 @@ let main () =
   let a = [|1.5; 2.0|] in
   (* let open Complex in
   let a : Complex.t array = [|{re = 1.5; im = 0.0}; {re = 2.0; im = 0.0}|] in *)
-  print_endline (Ops.Vec.Val.to_string (Ops.norm a))
+  print_endline (Ops.Val.to_string (Ops.norm a))
 
 let () = main ()
